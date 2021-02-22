d7a1e7f4036f765750a72df185e114914652c063
ibrunera <bteixeira010@gmail.com>
2021-02-22 09:54:13 -0300

    First Commit, criacao server.ts

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+/**
+ * GET: Buscar
+ * Post: Salvar/Criar
+ * Put: Alterar
+ * Delete
+ * Patch: Alteracao Especifica
+ */
+
+app.get('/', (request, response)=>{
+  return response.json({msg : 'Hello World NLW04!'});
+});
+
+app.post('/', (request, response)=>{
+  //Receber dados
+  return response.json({msg: 'Dados salvos com sucesso!'});
+});
+
+app.listen(3333, ()=>{
+  console.log('🚴 Server running at http://localhost:3333');
+});
