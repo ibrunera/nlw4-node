288ae0eb787adf768dfe9871011e0058150f4299
ibrunera <bteixeira010@gmail.com>
2021-02-24 20:17:48 -0300

    user,survey repositories and tests

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import {app} from '../app';
+import createConnection from '../database';
+
+describe('Users', ()=>{
+  beforeAll(async ()=>{
+    const connection = await createConnection();
+    await connection.runMigrations();
+
+  });
+  it('Should be able to create a new user', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name:'Bob Hope',
+        email:'bob@hope.com'
+      });
+      expect(response.status).toBe(201);
+  });
+
+  it('Should not be able to create a user with existing e-mail', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name:'Bob Hope',
+        email:'bob@hope.com'
+      });
+      expect(response.status).not.toBe(201);
+  })
+});
